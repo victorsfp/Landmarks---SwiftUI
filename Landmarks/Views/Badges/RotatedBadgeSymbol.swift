c1fd5dabff7616b4bc3a323f4749ade60b0e5c23
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2024-09-27 13:14:35 -0300

    Draw with GeometryReader and Path

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Victor dos Santos Feitosa on 27/09/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
